0664229724d6208fe9c948b0da20f96c3aff0c46
venkatesh <venkateshtm28@gmail.com>
2022-12-22 16:09:15 +0530

    22/12/2022 4:10PM

--- SocialMedia/WEB-INF/classes/com/ideas2it/action/Login.java
+++ SocialMedia/WEB-INF/classes/com/ideas2it/action/Login.java
@@ -1,10 +1,26 @@
 package com.ideas2it.action;
 
-import com.opensymphony.xwork2.ActionSupport;
 
-public class Login extends ActionSupport{
+import java.util.Map;  
+
+import com.opensymphony.xwork2.ActionSupport;
+  
+import org.apache.struts2.dispatcher.SessionMap;  
+import org.apache.struts2.interceptor.SessionAware;  
+
+import com.ideas2it.service.UserService;
+import com.ideas2it.service.serviceimpl.UserServiceImpl;
+import com.ideas2it.exception.CustomException;
+
+public class Login extends ActionSupport implements SessionAware {
     private String email;
     private String password;
+    private SessionMap<String, Object> sessionMap;
+    private UserService userService = new UserServiceImpl();
+    
+    public void setSession(Map<String, Object> map) {
+        sessionMap = (SessionMap) map;
+    }
 
     public void setEmail(String email) {
         this.email = email;
@@ -23,6 +39,16 @@ public class Login extends ActionSupport{
     }
     
     public String execute() {                 
-        return "success";   
+        String result = "invalid";
+        
+        try {
+            if (userService.isValidCredentials(email, password)) {
+                sessionMap.put("userId", userService.getUserId(email));
+                result = "success"; 
+            }  
+        } catch (CustomException customException) {
+            result = "error";
+        }   
+        return result;  
     }
 }
